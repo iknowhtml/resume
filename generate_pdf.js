48a309b8b59d454c8a253a37177384e828fff52a
Aki Gao <xag9bb@virginia.edu>
2017-07-18 23:06:18 -0400

    add node script to convert resume html to pdf

---
+++ generate_pdf.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const pdf = require('html-pdf');
+const { JSDOM } = require('jsdom');
+
+const options = {
+  directory: '/tmp',
+  base: `file://${__dirname}/`,
+  height: '1553px',
+  width: '1200px',
+  orientation: 'portrait'
+};
+
+const buildHtml = () => {
+  const style = fs.readFileSync('./cv.css', 'utf8');
+  const dom = new JSDOM(fs.readFileSync('./index.html', 'utf8'));
+
+  const body = dom.window.document.querySelector('body').innerHTML;
+
+  console.log('building html...');
+
+  return (
+    '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">' +
+    '<title>Xiaoyu A. Gao Resume</title>' +
+    '<style type="text/css">' +
+    style +
+    '</style>' +
+    '</head><body style="font-family:">' +
+    body +
+    '</body></html>'
+  );
+};
+
+const HTML = buildHtml();
+
+console.log('complete building html!');
+console.log('converting html to pdf...');
+pdf.create(HTML, options).toFile('./cv.pdf', (err, res) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(res);
+  console.log('complete converting html to pdf!');
+});
