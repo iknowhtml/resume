0bcccd84e782b87bbcd9cdc5fc7903ce5f136d81
iknowhtml <iknowsmtp@gmail.com>
2019-07-10 22:28:51 -0400

    Update PostCSS configuration file

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,10 @@ module.exports = ({ options, env }) => ({
   map: env === 'development' ? options.map : false,
   plugins: {
     'postcss-preset-env': {
-      stage: 4,
+      stage: 0,
+      features: {
+        'nesting-rules': true,
+      },
     },
   },
 });
