1bf1e31233d274bdce09e092269b3f93240afd52
iknowhtml <iknowsmtp@gmail.com>
2021-11-04 08:43:15 -0400

    Refactor to use import & top level await

--- utils/build_pdf.js
+++ utils/build_pdf.js
@@ -1,43 +1,39 @@
-const puppeteer = require('puppeteer');
-const path = require('path');
+import puppeteer from 'puppeteer';
+import path from 'path';
+import { writeFileSync } from 'fs';
 
-const generatePdf = async () => {
-  try {
-    console.log('launching headless browser...');
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
+try {
+  console.log('launching headless browser...');
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
 
-    console.log('opening new page...');
-    const page = await browser.newPage();
+  console.log('opening new page...');
+  const page = await browser.newPage();
 
-    console.log(`navigating to file://${path.resolve('dist', 'index.html')}`);
-    await page.goto(`file://${path.resolve('dist', 'index.html')}`, {
-      waitUntil: 'networkidle0',
-    });
+  console.log(`navigating to file://${path.resolve('dist', 'index.html')}`);
+  await page.goto(`file://${path.resolve('dist', 'index.html')}`, {
+    waitUntil: 'networkidle0',
+  });
 
-    console.log('generating pdf...');
-    console.log('writing pdf to disk...');
+  console.log('generating pdf...');
 
-    const defaultMargins = '0.39in';
-    const margin = ['right', 'bottom', 'left'].reduce(
-      (marginConfiguration, property) => ({
-        ...marginConfiguration,
-        [property]: defaultMargins,
-      }),
-      { top: '0.25in' }
-    );
-    await page.pdf({
-      path: path.resolve('dist', "Aki Gao's Resume.pdf"),
-      margin,
-    });
+  const defaultMargins = '0.39in';
+  const margin = ['right', 'bottom', 'left'].reduce(
+    (marginConfiguration, property) => ({
+      ...marginConfiguration,
+      [property]: defaultMargins,
+    }),
+    { top: '0.25in' }
+  );
+  const buffer = await page.pdf({
+    path: path.resolve('utils', "Aki Gao's Resume.pdf"),
+    margin,
+  });
 
-    await browser.close();
-    console.log('complete writing pdf to disk!');
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-generatePdf();
+  console.log('complete generating pdf!');
+  await browser.close();
+} catch (e) {
+  console.log(e);
+}
